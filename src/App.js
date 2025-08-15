de3603678d2e538cd28775f8d1cfc706e0321fdd
AlmasMalik <almasmalik107@gmail.com>
2025-08-15 18:37:39 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
+import Signup from "./Components/Signup/Signup";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import ChatHeader from "./Components/ChatHeader/ChatHeader";
 import ChatWindow from "./Components/ChatWindow/ChatWindow";
 import "./index.css";
 
 function App() {
-  return (
+  const [showSignup, setShowSignup] = useState(false);
+
+  // You can pass setShowSignup to Signup for switching after signup
+  return showSignup ? (
+    <Signup />
+  ) : (
     <div className="app">
       <Sidebar />
       <div className="chat-section">
